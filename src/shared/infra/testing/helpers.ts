335400004634cc3605cdd687be539c5b4c4f5f9b
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-08-19 13:04:15 -0300

    Added usecases

--- src/shared/infra/testing/helpers.ts
+++ src/shared/infra/testing/helpers.ts
@@ -5,7 +5,6 @@ export function setupSequelize(options: SequelizeOptions = {}) {
     let _sequelize: Sequelize;
 
     beforeAll(async () => {
-        console.log(Config.db())
         _sequelize = new Sequelize({
             ...Config.db(),
             ...options,
